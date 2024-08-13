ec9f7e5b8ab4cee7acdbb096aacef8070f346a88
bean0418 <84067859+bean0418@users.noreply.github.com>
2024-08-13 23:02:11 +0900

    Update func_app.py

--- test_project/func_app.py
+++ test_project/func_app.py
@@ -7,8 +7,8 @@ from ast import literal_eval
 import pickle
 
 # Load data
-df1 = pd.read_csv("tmdb_5000_credits.csv")
-df2 = pd.read_csv("tmdb_5000_movies.csv")
+df1 = pd.read_csv("test_project/tmdb_5000_credits.csv")
+df2 = pd.read_csv("test_project/tmdb_5000_movies.csv")
 
 # Rename columns for merging
 df1.columns = ['id', 'title', 'cast', 'crew']
